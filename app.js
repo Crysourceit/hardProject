9055852e16d5e9a5135ef89a5813da6c4a19f02a
Crysource <hohomona21@gmail.com>
2023-09-25 13:22:03 +0900

    Merge commit '3b0c633b401bcd693db10c0f275f65323e219312'
    # Conflicts:
    #	app.js
    #	index.html

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 console.log("chiken diner")
 console.log("leaning web dev")
+=======
+console.log("hello my dream")
+console.log("ngong")
+>>>>>>> 3b0c633b401bcd693db10c0f275f65323e219312
